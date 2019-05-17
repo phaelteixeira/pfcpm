b9a652118ad80dc813bc92cd59a9dcc37c359323
Alankl07 <alan.fsa.ans@hotmail.com>
2019-05-17 12:51:37 -0300

    Modificação no stylo da tela suspeito

--- pfcpm/resources/views/cadastrar_suspeito.blade.php
+++ pfcpm/resources/views/cadastrar_suspeito.blade.php
@@ -6,11 +6,11 @@
         <h1>Cadastro de Suspeitos</h1>
         <div id="div_susp">
             <div class="nomesus">
-                <label for="nomesus">Nome</label>
+                <label for="nomesus">Nome Completo:</label>
                 <input class="form-control" name="nome" id="nomesus" size=30>
             </div>
             <div class="vulgosus">
-                <label for="vulgosus">Vulgo</label>
+                <label for="vulgosus">Vulgo:</label>
                 <input class="form-control" name="vulgo" id="vulgosus"> 
             </div>
             <div class="fotosus">
@@ -18,35 +18,35 @@
                 <input type="file" name="foto" id="fotosus">
             </div>
             <div class="cpfsus">
-                <label for="cpfsus">CPF</label>
+                <label for="cpfsus">CPF:</label>
                 <input class="form-control" name="cpf" id="cpfsus">
             </div>
             <div class="rgsus">
-                <label for="rgsus">RG</label>
+                <label for="rgsus">RG:</label>
                 <input class="form-control" name="rg" id="rgsus">
             </div>
             <div class="cidadesus">
-                <label for="cidadesus">Cidade</label>
+                <label for="cidadesus">Cidade:</label>
                 <input class="form-control" name="cidade" id="cidadesus">
             </div>
             <div class="estadosus">
-                <label for="estadosus">Estado</label>
+                <label for="estadosus">Estado:</label>
                 <input class="form-control" name="estado" id="cidadesus">
             </div>
             <div class="crimesus">
-                <label for="crimesus">Tipo de Crimes</label>
+                <label for="crimesus">Tipo de Crimes:</label>
                 <input class="form-control" name="crime" id="crimesus">
             </div>
             <div class="atu">
-                <label for="localAtu">Local de Atuação</label>
+                <label for="localAtu">Local de Atuação:</label>
                 <input class="form-control" name="localAtuacao" id="localAtu">
             </div>
             <div class="datanasc">
-                <label for="crimesus">Data de Nascimento</label>
+                <label for="crimesus">Data de Nascimento:</label>
                 <input class="form-control" name="dataNascimento" id="datanasc">
             </div>
             <div class="sexo">
-                <label for="sexosus">Sexo</label><br>
+                <label for="sexosus">Sexo:</label><br>
                 <select id="sex" name="sexo">
                     <option name="sexo">-------</option>
                     <option name="sexo">Masculino</option>
@@ -54,11 +54,11 @@
                 </select>
             </div>
             <div class="pai">
-                <label for="pai">Nome do Pai</label>
+                <label for="pai">Nome do Pai:</label>
                 <input class="form-control" name="nomePai" id="pai">
             </div>
             <div class="mae">
-                <label for="mae">Nome da Mãe</label>
+                <label for="mae">Nome da Mãe:</label>
                 <input class="form-control" name="nomeMae" id="mae">
             </div>
             
